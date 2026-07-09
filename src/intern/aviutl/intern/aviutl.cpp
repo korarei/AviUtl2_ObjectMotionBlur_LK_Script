7e209a3be82a6dfac27046f4157abbe8d1512f1d
Korarei <132204522+korarei@users.noreply.github.com>
2026-07-10 08:10:53 +0900

    Refactor: Update Logger

--- src/intern/aviutl/intern/aviutl.cpp
+++ src/intern/aviutl/intern/aviutl.cpp
@@ -6,41 +6,31 @@ void Logger::Init(LOG_HANDLE* logger) { Instance().logger_ = logger; }
 void Logger::Log(const std::wstring& message) {
     auto* logger = Instance().logger_;
 
-    if (logger != nullptr) {
-        logger->log(logger, message.c_str());
-    }
+    logger->log(logger, message.c_str());
 }
 
 void Logger::Debug(const std::wstring& message) {
     auto* logger = Instance().logger_;
 
-    if (logger != nullptr) {
-        logger->verbose(logger, message.c_str());
-    }
+    logger->verbose(logger, message.c_str());
 }
 
 void Logger::Info(const std::wstring& message) {
     auto* logger = Instance().logger_;
 
-    if (logger != nullptr) {
-        logger->info(logger, message.c_str());
-    }
+    logger->info(logger, message.c_str());
 }
 
 void Logger::Warning(const std::wstring& message) {
     auto* logger = Instance().logger_;
 
-    if (logger != nullptr) {
-        logger->warn(logger, message.c_str());
-    }
+    logger->warn(logger, message.c_str());
 }
 
 void Logger::Error(const std::wstring& message) {
     auto* logger = Instance().logger_;
 
-    if (logger != nullptr) {
-        logger->error(logger, message.c_str());
-    }
+    logger->error(logger, message.c_str());
 }
 
 Logger& Logger::Instance() {
